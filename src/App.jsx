bc97ab5882a8910666a0afc9406dad485301a510
LuisSubiabre <luis.subiabre@gmail.com>
2024-07-22 10:44:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { MyNavbar } from "./components/Navbar";
+import { Home } from './components/Home';
+
+function App() {
+
+  return (
+    <>
+      <MyNavbar />
+      <Home />
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <>
+            <h1>!Pizzería Mamma Mia!</h1>
+        </>
+    );
+};
