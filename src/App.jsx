b111420249056e55b4b35b1fb8e28834ab946a05
LuisSubiabre <luis.subiabre@gmail.com>
2024-07-31 11:25:34 -0300

    new branch hito1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { MyNavbar } from "./components/Navbar";
 import { Home } from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <MyNavbar />
       <Home />
+      <Footer />
 
 
     </>
