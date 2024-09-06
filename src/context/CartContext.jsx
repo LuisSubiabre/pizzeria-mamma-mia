6fe3cb993aed4758b4e506614cb71a31b4b03c02
Luis <luis.subiabre@gmail.com>
2024-09-06 19:29:11 -0300

    context carrito

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,31 +1,64 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
     const [count, setCount] = useState(0);
-    const [carrito, setCarrito] = useState([]);
+    const [cart, setCart] = useState([]);
+
+
+    useEffect(() => {
+        calcularTotal();
+    }, [cart]);
 
     const addToCart = (element) => {
-        setCarrito((prevState) => {
+        setCart((prevState) => {
             const exist = prevState.find((x) => x.id === element.id);
-            let newCart;
+
             if (exist) {
-                newCart = prevState.map((x) => x.id === element.id ? { ...exist, cantidad: exist.cantidad + 1 } : x);
+                //setNewCart(prevState.map((x) => x.id === element.id ? { ...exist, cantidad: exist.cantidad + 1 } : x));
+                return prevState.map((x) => x.id === element.id ? { ...exist, cantidad: exist.cantidad + 1 } : x);
+
             } else {
-                newCart = [...prevState, { ...element, cantidad: 1 }];
+                // setNewCart([...prevState, { ...element, cantidad: 1 }]);
+                return [...prevState, { ...element, cantidad: 1 }];
             }
-            calcularTotal(newCart);
-            return newCart;
+            return prevState;
+
         });
     };
-    const calcularTotal = (carritoRender) => {
-        const nuevoTotal = carritoRender.reduce((a, c) => a + c.price * c.cantidad, 0);
-        setCount(nuevoTotal);
+
+
+    /* funcion para disminuir carrito  */
+    const removeFromCart = (element) => {
+        setCart((prevState) => {
+            const exist = cart.find((x) => x.id === element.id)
+
+            if (exist) {
+                //si el producto es 1 y el usuario da click se quita del carrito, ya que de 1 pasara a 0.
+                if (exist.cantidad <= 1) {
+                    return prevState.filter((x) => x.id !== element.id)
+
+                }
+                //si el producto es mayor a 1 se disminuye en 1
+                else {
+                    return prevState.map((x) => x.id === element.id ? { ...exist, cantidad: exist.cantidad - 1 } : x)
+                }
+
+            }
+            return prevState;
+        })
+    }
+
+
+    const calcularTotal = () => {
+        let nuevoValor = cart.reduce((a, c) => a + c.price * c.cantidad, 0);
+        setCount(nuevoValor);
+
     };
 
     return (
-        <CartContext.Provider value={{ count, setCount, carrito, setCarrito, addToCart }}>
+        <CartContext.Provider value={{ count, cart, addToCart, removeFromCart }}>
             {children}
         </CartContext.Provider>
     );
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,54 +3,36 @@ import { cartaPizza } from "../pizzas.js";
 import { Button, Container, ListGroup, Badge, Image } from 'react-bootstrap'
 import { formatNumber } from '../scripts.js';
 import { CartContext } from '../context/CartContext.jsx';
+
 const Cart = () => {
-    const { carrito } = useContext(CartContext);
+    const { cart, addToCart, removeFromCart, count } = useContext(CartContext);
 
-
-    /* funcion para disminuir carrito  */
-    const removeFromCart = (element) => {
-        setCarrito((prevState) => {
-            const exist = carrito.find((x) => x.id === element.id)
-
-            if (exist) {
-                //si el producto es 1 y el usuario da click se quita del carrito, ya que de 1 pasara a 0.
-                if (exist.cantidad <= 1) {
-                    return prevState.filter((x) => x.id !== element.id)
-
-                }
-                //si el producto es mayor a 1 se disminuye en 1
-                else {
-                    return prevState.map((x) => x.id === element.id ? { ...exist, cantidad: exist.cantidad - 1 } : x)
-                }
-
-            }
-            return prevState
-        })
-    }
-
-    const total = carrito.reduce((a, c) => a + c.price * c.cantidad, 0)
-    const HorizontalLine = ({ color }) => (
-        <div
-            style={{
-                borderBottom: `1px solid ${color}`,
-                margin: '10px 0',
-                padding: 2,
-            }}
-        />
-    );
     return (
         <>
             <h2>Carrito</h2>
 
             <Container>
                 <h4>Total del carrito: </h4>
-                {carrito.map((element) => (
-                    <li key={element.id}>
-                        {element.name} {formatNumber(element.price)} x {element.cantidad}
-                    </li>
+                <ListGroup>
+                    {cart.map((element) => (
+                        <ListGroup.Item key={element.id} className='p-2'>
+                            <Image
+                                src={element.image}
+                                style={{ width: '10%' }}
+                                rounded />
 
-                ))}
-                {total === 0 ? <p>El carrito esta vacio</p> : <div><p>TOTAL: {formatNumber(total)}</p> <Button variant='success'>Ir a pagar</Button></div>}
+                            <strong> {element.name} : </strong>{formatNumber(element.price)}
+
+                            <Button variant='outline-secondary' onClick={() => removeFromCart(element)}>-</Button>
+                            {element.cantidad}
+                            <Button variant='outline-secondary' onClick={() => addToCart(element)}>+</Button>
+
+
+                        </ListGroup.Item>
+
+                    ))}
+                </ListGroup>
+                {count === 0 ? <p>El carrito esta vacio</p> : <div><p>TOTAL: {formatNumber(count)}</p> <Button variant='success'>Ir a pagar</Button></div>}
 
 
             </Container>
