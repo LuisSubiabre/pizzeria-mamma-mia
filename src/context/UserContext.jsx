28ca5856fa3cf2fec207f3a29ca8855b62b7be01
Luis <luis.subiabre@gmail.com>
2025-02-04 11:50:49 -0300

    render backend update

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,101 +1,112 @@
-import { createContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { showAlert } from '../utils/helpers';
+import { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { showAlert } from "../utils/helpers";
 
 export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-    const [auth, setAuth] = useState(false);
-    const [user, setUser] = useState(null);
-    const navigate = useNavigate();
+  const [auth, setAuth] = useState(false);
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
-    // Verifica si hay un token al cargar la aplicación
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            profile();
+  // Verifica si hay un token al cargar la aplicación
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      profile();
+    }
+  }, []);
+
+  // Metodo para iniciar sesión
+  const login = async (email, password) => {
+    try {
+      const response = await fetch(
+        "https://simple-api-backend-nodejs-express-fs.onrender.com/api/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
         }
-    }, []);
+      );
 
-    // Metodo para iniciar sesión
-    const login = async (email, password) => {
-        try {
-            const response = await fetch("http://localhost:5000/api/auth/login", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ email, password }),
-            });
+      const data = await response.json();
+      if (data?.error) {
+        showAlert("error", data.error);
+        return;
+      }
 
-            const data = await response.json();
-            if (data?.error) {
-                showAlert('error', data.error);
-                return;
-            }
+      localStorage.setItem("token", data.token);
+      setAuth(true);
+      showAlert("success", "Logeado correctamente");
 
-            localStorage.setItem("token", data.token);
-            setAuth(true);
-            showAlert('success', 'Logeado correctamente');
+      navigate("/profile");
+    } catch (error) {
+      showAlert("error", "Error al iniciar sesión");
+    }
+  };
 
-            navigate('/profile');
-        } catch (error) {
-            showAlert('error', 'Error al iniciar sesión');
+  // Metodo para registrar un usuario
+  const register = async (email, password) => {
+    try {
+      const response = await fetch(
+        "https://simple-api-backend-nodejs-express-fs.onrender.com/api/auth/register",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
         }
-    };
+      );
+      const data = await response.json();
+      if (data?.error) {
+        showAlert("error", data.error);
+        return;
+      }
 
-    // Metodo para registrar un usuario
-    const register = async (email, password) => {
-        try {
-            const response = await fetch("http://localhost:5000/api/auth/register", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ email, password }),
-            });
-            const data = await response.json();
-            if (data?.error) {
-                showAlert('error', data.error);
-                return;
-            }
+      localStorage.setItem("token", data.token);
+      setAuth(true);
+      showAlert("success", "Registro exitoso");
+      navigate("/profile");
+    } catch (error) {
+      showAlert("error", "Error al registrarse");
+    }
+  };
 
-            localStorage.setItem("token", data.token);
-            setAuth(true);
-            showAlert('success', 'Registro exitoso');
-            navigate('/profile');
-        } catch (error) {
-            showAlert('error', 'Error al registrarse');
-        }
-    };
+  // Metodo para cerrar sesión
+  const logout = () => {
+    setAuth(false);
+    localStorage.removeItem("token");
+    showAlert("info", "Sesión finalizada").then(() => navigate("/"));
+  };
 
-    // Metodo para cerrar sesión
-    const logout = () => {
-        setAuth(false);
-        localStorage.removeItem('token');
-        showAlert('info', 'Sesión finalizada').then(() => navigate('/'));
-    };
+  // Metodo para obtener el perfil del usuario
+  const profile = async () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setAuth(true);
+      try {
+        const response = await fetch(
+          "https://simple-api-backend-nodejs-express-fs.onrender.com/api/auth/me",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+    }
+  };
 
-    // Metodo para obtener el perfil del usuario
-    const profile = async () => {
-        const token = localStorage.getItem("token");
-        if (token) {
-            setAuth(true);
-            try {
-                const response = await fetch("http://localhost:5000/api/auth/me", {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
-                const data = await response.json();
-                setUser(data);
-            } catch (error) {
-                console.error("Error fetching user:", error);
-            }
-        }
-    };
-
-    return (
-        <UserContext.Provider value={{ logout, login, auth, register, profile, user }}>
-            {children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider
+      value={{ logout, login, auth, register, profile, user }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export default UserProvider;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,81 +1,98 @@
-import React, { useState, useContext } from 'react';
-import { cartaPizza } from "../pizzas.js";
-import { Button, Container, ListGroup, Badge, Image } from 'react-bootstrap';
-import { formatNumber } from '../scripts.js';
-import { CartContext } from '../context/CartContext.jsx';
-import { UserContext } from '../context/UserContext.jsx';
-import Nav from 'react-bootstrap/Nav';
-import { Link } from 'react-router-dom';
-import { showAlert } from '../utils/helpers';
+import { useContext } from "react";
+import { Button, Container, ListGroup, Image } from "react-bootstrap";
+import { formatNumber } from "../scripts.js";
+import { CartContext } from "../context/CartContext.jsx";
+import { UserContext } from "../context/UserContext.jsx";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+import { showAlert } from "../utils/helpers";
 
 const Cart = () => {
-    const { cart, setCart, addToCart, removeFromCart, count, pedido, setPedido } = useContext(CartContext);
-    const { auth } = useContext(UserContext);
+  const { cart, setCart, addToCart, removeFromCart, count, pedido, setPedido } =
+    useContext(CartContext);
+  const { auth } = useContext(UserContext);
 
-    const checkout = async () => {
-        if (cart.length > 0) {
-            const response = await fetch('http://localhost:5000/api/checkouts', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`
-                },
-                body: JSON.stringify({ cart }),
-            });
-
-            if (!response.ok) {
-                throw new Error("Error en el proceso de compra");
-            }
-            const data = await response.json();
-            console.log('Checkout exitoso:', data);
-
-            // Genera un nuevo código de seguimiento
-            const trackingCode = Date.now();
-            setPedido(trackingCode); // Actualiza el estado con el nuevo código
-
-            // Muestra la alerta con el código actualizado
-            showAlert('success', 'Compra exitosa. Tu pedido llegará en 30 minutos. Código de seguimiento: ' + trackingCode);
-
-            setCart([]); // Limpia el carrito
+  const checkout = async () => {
+    if (cart.length > 0) {
+      const response = await fetch(
+        "https://simple-api-backend-nodejs-express-fs.onrender.com/api/checkouts",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify({ cart }),
         }
-    }
+      );
 
-    return (
-        <>
-            <Container>
-                <h4>Total del carrito: </h4>
-                <ListGroup>
-                    {cart.map((element) => (
-                        <ListGroup.Item key={element.id} className='p-2'>
-                            <Image
-                                src={element.image}
-                                style={{ width: '10%' }}
-                                rounded />
-                            <strong> {element.name} : </strong>{formatNumber(element.price)}
-                            <Button variant='outline-secondary' onClick={() => removeFromCart(element)}>-</Button>
-                            {element.cantidad}
-                            <Button variant='outline-secondary' onClick={() => addToCart(element)}>+</Button>
-                        </ListGroup.Item>
-                    ))}
-                </ListGroup>
-                {count === 0 ? (
-                    <p>El carrito está vacío</p>
-                ) : (
-                    <div>
-                        <p>TOTAL: {formatNumber(count)}</p>
-                        {auth ? (
-                            <Button variant='success' onClick={checkout}>Ir a pagar</Button>
-                        ) : (
-                            <Nav.Link as={Link} to="/login">
-                                <Button variant='danger'>Inicia sesión para pagar</Button>
-                            </Nav.Link>
-                        )}
-                    </div>
-                )}
-                {pedido && <p>Tu pedido {pedido} se encuentra en camino.</p>}
-            </Container>
-        </>
-    );
-}
+      if (!response.ok) {
+        throw new Error("Error en el proceso de compra");
+      }
+      const data = await response.json();
+      console.log("Checkout exitoso:", data);
+
+      // Genera un nuevo código de seguimiento
+      const trackingCode = Date.now();
+      setPedido(trackingCode); // Actualiza el estado con el nuevo código
+
+      // Muestra la alerta con el código actualizado
+      showAlert(
+        "success",
+        "Compra exitosa. Tu pedido llegará en 30 minutos. Código de seguimiento: " +
+          trackingCode
+      );
+
+      setCart([]); // Limpia el carrito
+    }
+  };
+
+  return (
+    <>
+      <Container>
+        <h4>Total del carrito: </h4>
+        <ListGroup>
+          {cart.map((element) => (
+            <ListGroup.Item key={element.id} className="p-2">
+              <Image src={element.image} style={{ width: "10%" }} rounded />
+              <strong> {element.name} : </strong>
+              {formatNumber(element.price)}
+              <Button
+                variant="outline-secondary"
+                onClick={() => removeFromCart(element)}
+              >
+                -
+              </Button>
+              {element.cantidad}
+              <Button
+                variant="outline-secondary"
+                onClick={() => addToCart(element)}
+              >
+                +
+              </Button>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+        {count === 0 ? (
+          <p>El carrito está vacío</p>
+        ) : (
+          <div>
+            <p>TOTAL: {formatNumber(count)}</p>
+            {auth ? (
+              <Button variant="success" onClick={checkout}>
+                Ir a pagar
+              </Button>
+            ) : (
+              <Nav.Link as={Link} to="/login">
+                <Button variant="danger">Inicia sesión para pagar</Button>
+              </Nav.Link>
+            )}
+          </div>
+        )}
+        {pedido && <p>Tu pedido {pedido} se encuentra en camino.</p>}
+      </Container>
+    </>
+  );
+};
 
 export default Cart;
